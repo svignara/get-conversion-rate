4de2f4b7a27df3c3d693b8b9a41d8b7243f98be1
Suvethan Vignarajah <suvethanvignarajah@oracle.local>
2016-05-07 00:04:22 -0400

    allowing for CORS

--- get-conversion-rate.js
+++ get-conversion-rate.js
@@ -8,7 +8,7 @@ var jsdom = require('jsdom'),
 
 //CORS middleware, only allow from svignara.github.io
 router.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'svignara.github.io');
+    res.header('Access-Control-Allow-Origin', '*://svignara.github.io');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
